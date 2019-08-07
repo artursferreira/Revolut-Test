89725d796d14d5d5750a151880cf8df33d494595
artur <artur@usemobile.xyz>
2019-08-06 23:55:00 -0300

    currencyviewmodel fix

--- app/src/main/java/com/artur/exchangecurrencies/network/CurrencyApi.kt
+++ app/src/main/java/com/artur/exchangecurrencies/network/CurrencyApi.kt
@@ -3,7 +3,7 @@ package com.artur.exchangecurrencies.network
 import com.artur.exchangecurrencies.model.RateResponse
 import io.reactivex.Observable
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 /**
  * The interface which provides methods to get result of webservices
@@ -13,5 +13,5 @@ interface CurrencyApi {
      * Get the list of the pots from the API
      */
     @GET("/latest?base={id}")
-    fun getCurrencies(@Path("id") base: String): Observable<RateResponse>
+    fun getCurrencies(@Query("id") base: String): Observable<RateResponse>
 }
--- app/src/main/java/com/artur/exchangecurrencies/ui/currency/CurrencyViewModel.kt
+++ app/src/main/java/com/artur/exchangecurrencies/ui/currency/CurrencyViewModel.kt
@@ -2,12 +2,20 @@ package com.artur.exchangecurrencies.ui.currency
 
 import androidx.lifecycle.MutableLiveData
 import com.artur.exchangecurrencies.base.BaseViewModel
+import com.artur.exchangecurrencies.model.RateResponse
+import com.artur.exchangecurrencies.network.CurrencyApi
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 /**
  * Created by Artur on 06/08/2019.
  */
 class CurrencyViewModel : BaseViewModel() {
+
+    @Inject
+    lateinit var currencyApi: CurrencyApi
 
     val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
     val errorMessage: MutableLiveData<Int> = MutableLiveData()
@@ -15,7 +23,7 @@ class CurrencyViewModel : BaseViewModel() {
     private lateinit var subscription: Disposable
 
     init {
-
+        getCurrencies("EUR")
     }
 
     override fun onCleared() {
@@ -24,6 +32,22 @@ class CurrencyViewModel : BaseViewModel() {
     }
 
     private fun getCurrencies(base: String) {
+
+        subscription = currencyApi.getCurrencies(base)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+
+                .subscribe(
+                        { result -> onCurrencyResultSuccess(result) },
+                        { onCurrencyResultError() }
+                )
+    }
+
+    private fun onCurrencyResultSuccess(result: RateResponse) {
+
+    }
+
+    private fun onCurrencyResultError() {
 
     }
 
